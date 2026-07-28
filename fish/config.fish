c2356392c61b9e85db8702e90c5c1d5479ed599a
Denis Chupritskiy <d.chupritskiy@skytech.ooo>
2026-07-28 03:37:33 +0300

    fish

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="exa --icons"
+alias vi="nvim"
+alias vim="nvim"
+alias tmux="/home/denis/bin/wtmux.sh"
+alias todo="nvim ~/.TODO"
+
+# opencode
+fish_add_path /home/denis/.opencode/bin
